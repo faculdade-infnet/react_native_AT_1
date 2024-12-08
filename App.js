2adeb100d216d219a99350eb0ade989f24d19c45
Samuel Hermany <samuelhermany@hotmail.com>
2024-12-08 11:58:19 -0300

    Initial commit - Inicio, projeto limpo - Inicio limpo e rodando
    Initial commit
    
    Inicio, projeto limpo
    
    Inicio limpo e rodando

--- App.js
+++ App.js
@@ -1,27 +1,19 @@
-import { Text, SafeAreaView, StyleSheet } from 'react-native';
-
-// You can import supported modules from npm
-import { Card } from 'react-native-paper';
-
-// or any files within the Snack
-import AssetExample from './components/AssetExample';
+import { Text, View, StyleSheet } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Text >
+        Home
+      </Text>     
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
     justifyContent: 'center',
     backgroundColor: '#ecf0f1',
     padding: 8,
