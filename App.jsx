287dac4c5eb5c1029b0af82b4e37364501cbbd3d
Samuel Hermany <samuelhermany@hotmail.com>
2024-12-08 17:44:40 -0300

    wip: Tela login finalizada

--- App.jsx
+++ App.jsx
@@ -1,29 +1,23 @@
 // #region Imports
 import TransacaoListScreen from './screens/TransacaoListScreen';
 import TransacaoFormScreen from './screens/TransacaoFormScreen';
-import HomeScreen from './screens/LoginScreen';
-// import LoginScreen from './screens/LoginScreen'; // Importe a tela de login
+import LoginScreen from './screens/LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 // #endregion Imports
 
 // Objeto do tipo stack para navegação
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      {/* Stack Navigator para controlar a navegação inicial */}
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {/* Tela de Login */}
-        <Stack.Screen name="Login" component={HomeScreen} />
-        
+      <Stack.Navigator screenOptions={{ headerShown: false }}>        
+        <Stack.Screen name="login" component={LoginScreen} />        
         {/* Navegação do menu */}
-        <Stack.Screen name="Main" component={DrawerNavigator} />
+        <Stack.Screen name="home" component={DrawerNavigator} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -32,21 +26,16 @@ export default function App() {
 // Navegação com o Drawer (menu)
 function DrawerNavigator() {
   return (
-    <Drawer.Navigator>
+    <Drawer.Navigator>      
       <Drawer.Screen 
-        name='home' 
-        options={{ title: "Home" }}
-        component={HomeScreen}
+        name='transacaoList' 
+        options={{ title: "Lista de Transasções" }}
+        component={TransacaoListScreen}
       />
       <Drawer.Screen 
-        name='home1' 
-        options={{ title: "Home1" }}
-        component={HomeScreen}
-      />
-      <Drawer.Screen 
-        name='home2' 
-        options={{ title: "Home2" }}
-        component={HomeScreen}
+        name='transacaoForm' 
+        options={{ title: "Fomulario" }}
+        component={TransacaoFormScreen}
       />
     </Drawer.Navigator>
   );
--- screens/LoginScreen/index.jsx
+++ screens/LoginScreen/index.jsx
@@ -1,10 +1,46 @@
-import { View, Text, StyleSheet, TextInput } from 'react-native';
+import { useState } from 'react';
+import { View, Text, Image, StyleSheet, TextInput, Pressable } from 'react-native';
 
-export default function HomeScreen() {   
+export default function HomeScreen ({ navigation }){  
+  const [userName, setUserName] = useState("");
+  const [userSenha, setUserSenha] = useState("");
+
+  const imgUser = 'https://img.freepik.com/vetores-premium/icone-de-circulo-de-usuario-anonimo-estilo-simples-de-ilustracao-vetorial-com-sombra-longa_520826-1931.jpg?w=740'
   
+  const verifyLogin = () => {
+    if(userName == 'admin' && userSenha == '00'){
+      navigation.navigate('home');
+    }
+  }
+
   return (    
     <View style={styles.container}>
-      <Text>Login</Text>      
+      <View style={styles.containerImage}>
+        <Image
+          source={{ uri: imgUser }} 
+          style={styles.image} 
+        />
+      </View>
+      <View style={styles.containerChild}>
+        <TextInput style={[styles.textInput, styles.text]}
+          placeholder='Nome'
+          keyboardType='default'
+          value={userName}
+          onChangeText={setUserName}
+        />
+        <TextInput style={[styles.textInput, styles.text]}
+          placeholder='Senha'
+          keyboardType='default'
+          secureTextEntry={true}
+          value={userSenha}
+          onChangeText={setUserSenha}
+        />        
+        <Pressable style={ styles.button }                                                
+            onPress={verifyLogin}
+        >
+          <Text style={styles.text}>Acessar</Text>
+        </Pressable>
+      </View>
     </View>
   )
 }
@@ -12,5 +48,45 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  }
+    alignItems: "center",
+    justifyContent: "center",    
+    backgroundColor: "#fff",     
+  },
+  containerImage: {
+    marginBottom: 100,
+  }, 
+  image: {
+    width: 250,
+    height: 250,
+    backgroundColor: "transparent",
+  },
+  containerChild:{
+    width: '100%',
+    justifyContent: "flex-end"
+  },
+  textInput: {
+    padding: 10,
+    marginHorizontal: 10,
+    marginVertical: 5,
+    backgroundColor: "#e6e8e9",
+    borderBottomWidth: 1,
+    borderBottomColor: "#3e3e3e",
+    borderRadius: 5,
+    textAlign: "center",
+  },  
+  button: {
+    marginHorizontal: 10,
+    marginTop: 25,    
+    height: 50,
+    borderBottomWidth: 1,
+    borderBottomColor: "#3e3e3e",
+    borderRadius: 5,
+    backgroundColor: "#f4a261",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text:{
+    fontSize: 20, 
+    justifyContentHorizontal: "center",    
+  }  
 })
